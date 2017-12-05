79b93603251fbe0dbcfa5e3a29da33e2de32ccf0
Noah Gottlieb and Nate McGregor <noah@>
2017-12-04 16:48:19 -0800

    add admin route for adding new project and display

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,23 @@
 class ProductsController < ApplicationController
+  before_action :authorize, only: [:show]
+  before_action :authorize_admin, only: [:new, :create, :show]
   def index
     @products = Product.all
     @order_item = current_order.order_items.new
+  end
+
+  def new
+    @product = Product.find(params[:id])
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice]= "Product Created!"
+      redirect_to products_path
+    else
+      render :new
+    end
   end
 
   def show
