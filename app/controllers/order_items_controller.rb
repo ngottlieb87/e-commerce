10fc7f997a3a92a96f82c0ac89d59a901f97e8c6
Noah Gottlieb and Nate McGregor <nate@>
2017-12-06 15:17:00 -0800

    update cart to sum item quantities

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -2,11 +2,20 @@ class OrderItemsController < ApplicationController
 
   def create
     @order = current_order
-    @item = @order.order_items.new(item_params)
-    @order.account_id = current_user.id
-    @order.save
-    session[:order_id] = @order.id
-    redirect_to products_path
+    if @order.order_items.exists?(:product_id => item_params[:product_id])
+      @item = @order.order_items.where(:product_id => item_params[:product_id])
+      quan = @item.first.quantity += item_params[:quantity].to_i
+      @item.update(:quantity => quan)
+      @order.save
+      session[:order_id] = @order.id
+      redirect_to products_path
+    else
+      @item = @order.order_items.new(item_params)
+      @order.account_id = current_user.id
+      @order.save
+      session[:order_id] = @order.id
+      redirect_to products_path
+    end
   end
 
   def destroy
@@ -14,7 +23,7 @@ class OrderItemsController < ApplicationController
     @item = @order.order_items.find(params[:id])
     @item.destroy
     @order.save
-    redirect_to cart_path
+    redirect_to '/'
   end
 
   private
