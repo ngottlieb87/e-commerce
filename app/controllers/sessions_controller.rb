5bbb75ae1b278730b41bb9d72334b38ef51acc24
Noah Gottlieb and Nate McGregor <nate@>
2017-12-06 11:49:09 -0800

    ajax for sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,13 @@ class SessionsController < ApplicationController
   def create
     @user = User.authenticate(params[:email], params[:password])
     if @user
+
       flash[:notice] = "You've signed in."
       session[:user_id] = @user.id
       redirect_to products_path
     else
       flash[:alert] = "There was a problem signing in. Please try again."
-      redirect_to signin_path
+      redirect_to '/'
     end
   end
 
